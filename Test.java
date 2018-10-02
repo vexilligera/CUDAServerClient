6aed6695584b0e064108b2ec137ebf2e417e79c4
vexilligera <vexilligera@gmail.com>
2018-10-02 14:40:19 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,18 @@
 public class Test {
 	public static void main(String[] args) {
-		SparkCUDAClient client = new SparkCUDAClient("127.0.0.1", 2333, "./", "test0", 1024);
-		byte[] array = new String("Some data").getBytes();
+		// 1GB data
+		int maxSize = 1 * 1024 * 1024 * 1024;
+		System.out.printf("Testing file size of %d\n", maxSize);
+		SparkCUDAClient client = new SparkCUDAClient("127.0.0.1", 2333, "./", "test0", maxSize);
+		byte[] array = new byte[maxSize];
+		array[0] = 65;
+		array[1] = 0;
+		array[maxSize - 1] = 1;
+		long begin = System.nanoTime();
 		client.swapToGPU(array);
+		long end = System.nanoTime();
+		long time = end - begin;
+		System.out.printf("Time elapsed: write %d. nanoseconds\n", time);
+		client.close();
 	}
 }
